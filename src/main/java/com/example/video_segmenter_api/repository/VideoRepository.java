cd3574bf3e2dda1dc18ca58c0384e1fb8a9c8be2
Matthieu PIERRE LOUIS <matthieu.pierrelouis@ext.canal-plus.com>
2026-02-10 02:28:45 +0100

    feat: add repository (ddd)

--- src/main/java/com/example/video_segmenter_api/repository/VideoRepository.java
+++ src/main/java/com/example/video_segmenter_api/repository/VideoRepository.java
@@ -5,9 +5,20 @@ import com.example.video_segmenter_api.domain.VideoUpload;
 
 import java.util.UUID;
 
+import java.util.Optional;
+
 public interface VideoRepository {
 
     void save(VideoUpload video);
 
     void updateStatus(UUID id, VideoStatus status);
+
+    Optional<VideoUpload> findById(UUID id);
+
+    void updateMetadata(
+            UUID id,
+            double duration,
+            int segmentsCount,
+            VideoStatus status
+    );
 }
